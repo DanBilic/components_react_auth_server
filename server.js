89626542f6656b35c29a7609e7586187919607e8
D1234B <bilic88@gmx.de>
2020-07-26 18:21:49 +0200

    added several security middlewares

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const cors = require("cors");
 const errorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+
+//security middlewares
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 //load env variables
 dotenv.config({ path: "./config/config.env" });
@@ -18,6 +26,28 @@ const app = express();
 
 //Body parser
 app.use(express.json());
+
+//sanitize mongo data
+app.use(mongoSanitize());
+
+//set security headers
+app.use(helmet());
+
+//prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+
+//prevent http param polution
+app.use(hpp());
+
+//enable CORS
+app.use(cors());
 
 //cookie parser
 app.use(cookieParser());
